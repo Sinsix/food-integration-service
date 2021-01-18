c13d6947b270e458fb62163694fac30348317127
MatheusXavier15 <matheusxavier055@gmail.com>
2021-01-18 18:28:31 -0300

    Correção da pagina de registro, centralização e fatoração

--- src/components/RegisterPanel/index.jsx
+++ src/components/RegisterPanel/index.jsx
@@ -11,12 +11,12 @@ const RegisterPanel = (props) => {
             <div className="Register">
                 <h1>Registro</h1>
                 <form action=" ">
-                    <input type="text" placeholder="Nome" id="Name" name="Name" />
-                    <input type="text" placeholder="Sobrenome" id="Surname" name="Surname" />
-                    <input type="text" placeholder="Nome de Usuário" id="Surname" name="Surname" />
-                    <input type="password" placeholder="Senha" className="UserPassword" name="UserPassword" />
-                    <input type="password" placeholder="Confirme a senha" className="UserPassword" name="UserPassword" />
-                    <input type="email" placeholder="E-mail" id="email" name="email"/>
+                    <input type="text" className="FormInput" placeholder="Nome" id="Name" name="Name" />
+                    <input type="text" className="FormInput" placeholder="Sobrenome" id="Surname" name="Surname" />
+                    <input type="text" className="FormInput" placeholder="Nome de Usuário" id="Surname" name="Surname" />
+                    <input type="password" className="FormInput" placeholder="Senha" name="UserPassword" />
+                    <input type="password" className="FormInput" placeholder="Confirme a senha" name="UserPassword" />
+                    <input type="email" className="FormInput" placeholder="E-mail" id="email" name="email"/>
                     <div id="Terms">
                     <input type="checkbox" id="TermsConfirm" name="TermsConfirm" required/>
                     <label htmlFor="TermsConfirm">Você concorda com os termos e codições?</label>
