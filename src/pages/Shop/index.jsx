365147c91c52ac2e6b12f5eddd8e1436942e8b68
Pedro Vilaça <pedrovilacav@gmail.com>
2021-03-06 20:52:02 -0300

    Colocado produtos na tela inicial para teste e pequenas correções

--- src/pages/Shop/index.jsx
+++ src/pages/Shop/index.jsx
@@ -1,14 +1,29 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import NavBar from './../../components/NavBar'
 import CartFooter from './../../components/CartFooter'
+import Product from './../../components/Product'
+
+import './styles.scss'
 
 function Shop () {
   const operator = ['Marcos da Silva']
   return (
-      <Fragment>
+      <div className="ShopPage">
           <NavBar operator={operator}></NavBar>
+          <div className="Container">
+            <div className="Topics">
+              que que pega
+            </div>
+            <div className="Products">
+              <Product title="Teste product"/>
+              <Product title="Teste product"/>
+              <Product title="Teste product"/>
+              <Product title="Teste product"/>
+              <Product title="Teste product"/>
+            </div>
+          </div>
           <CartFooter />
-      </Fragment>
+      </div>
   )
 }
 
